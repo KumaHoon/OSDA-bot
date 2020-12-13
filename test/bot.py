bca16e6b895e66a4d5e896b98f4e5fbf1f609b10
leewontae <dldnjsxo00524@naver.com>
2020-12-13 16:29:15 +0900

    Change client -> bot , Print "{Discord_bot} has connected to Discord!"

--- test/bot.py
+++ test/bot.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import discord
+from discord.ext import commands
 from dotenv import load_dotenv
 import asyncio
 import random
@@ -15,37 +16,10 @@ load_dotenv(os.path.join(BASEDIR, '.env'))
 TOKEN = os.getenv('DISCORD_TOKEN')
 GUILD = os.getenv('DISCORD_GUILD')
 
-client = discord.Client()
+bot = commands.Bot(command_prefix = '>>')
 
-@client.event
+@bot.event
 async def on_ready():
-    for guild in client.guilds:
-        if guild.name == GUILD:
-            break
+    print(f'{bot.user.name} has connected to Discord!')
 
-    print(
-        f'{client.user} is connected to the following guild:\n'
-        f'{guild.name}(id: {guild.id})'
-    )
-
-@client.event
-async def on_message(message):
-    if message.author == client.user:
-        return
-
-    brooklyn_99_quotes = [
-        'I\'m the human form of the 💯 emoji.',
-        'Bingpot!',
-        (
-            'Cool. Cool cool cool cool cool cool cool, '
-            'no doubt no doubt no doubt no doubt.'
-        ),
-    ]
-
-    if message.content == '99!':
-        response = random.choice(brooklyn_99_quotes)
-        await message.channel.send(response)
-
-
-
-client.run(TOKEN)
+bot.run(TOKEN)
